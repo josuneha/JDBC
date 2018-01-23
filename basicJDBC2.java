4333c377fbfd1b2bb5a6b4113d3fb1e0c4b7de65
Josune <jhernantes@tecnun.es>
2018-01-23 08:41:31 +0100

    Update basicJDBC2.java

--- basicJDBC2.java
+++ basicJDBC2.java
@@ -3,29 +3,29 @@ class basicJDBC2 {
 
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
         
-		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
         
-		Connection connection = DriverManager.getConnection("jdbc:odbc:northbrick");
+        Connection connection = DriverManager.getConnection("jdbc:odbc:northbrick");
         
-		Statement statement = connection.createStatement();
-		
-		// You should write the sql to check if there is a problem
-		String sql="Select * from Employees WHERE EmployeeID='10'";
-		System.out.println("sql: " + sql);
-		
-		ResultSet result = statement.executeQuery(sql);
-		
+        Statement statement = connection.createStatement();
         
-		while(result.next()) {
+        // You should write the sql to check if there is a problem
+        String sql="Select * from Employees WHERE EmployeeID='10'";
+        System.out.println("sql: " + sql);
+        
+        ResultSet result = statement.executeQuery(sql);
+        
+        
+        while(result.next()) {
             System.out.print(result.getString("LastName")+", ");
             System.out.println(result.getString("FirstName")+ " , ");
-			System.out.println(result.getString("City"));
+            System.out.println(result.getString("City"));
         }
-		
-		result.close();
-		statement.close();
-		
+        
+        result.close();
+        statement.close();
+        
         connection.close();
     }
-	
+    
 }
